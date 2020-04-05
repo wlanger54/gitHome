56f19a2508dfcd0ddeb60736f7c40b308b382164
wlanger54 <wlanger54@gmail.com>
2020-04-05 21:14:10 +0300

    Check commit 5

--- src/test/java/ATDTest/Accessories_MainPage/MainPageTest.java
+++ src/test/java/ATDTest/Accessories_MainPage/MainPageTest.java
@@ -12,7 +12,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class MainPageTest {
 
-
+// !!!!!!!!!!!!!!!!!!!!!!
     @BeforeMethod
     public void setUp() {
         Configuration.startMaximized = true;
